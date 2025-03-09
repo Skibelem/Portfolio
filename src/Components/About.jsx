f7245abcc69ef6389330bba3d101ddeace3da31e
Skibelem <adelugba.joel@gmail.com>
2025-03-09 13:17:16 +0100

    Update About.jsx
    I changed something in the About section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,17 +5,14 @@ const About = () => {
     <section id="about" className="p-10 bg-white text-center">
       <h2 className="text-2xl font-bold">About Me</h2>
       <p className="text-gray-600 mt-2">
-        Hi, I'm Joel Adelugba {"</joe>"} – a Professional Web Developer with 3+ Years of Experience.
+        Hi, I'm Joel Adelugba {"</joe>"} – a Professional Web Developer with 2+ Years of Experience.
 
         Over the past 3 years, I’ve helped businesses and individuals bring their ideas to life through modern, scalable, and high-performance web solutions.
 
         What I Do:
 
         ⚡ Frontend Development – Creating sleek, user-friendly, and responsive designs with React.js, Tailwind CSS, and JavaScript.
-        🔥 Backend Development – Building powerful and secure APIs using Node.js, Express, and MongoDB.
-        🎨 UI/UX Design – Crafting visually appealing and interactive user interfaces.
         🚀 Performance Optimization – Ensuring fast load times, SEO-friendly structures, and high accessibility standards.
-        🔐 Security & Best Practices – Implementing authentication, data protection, and modern coding standards.
         I am passionate about creating seamless digital experiences and constantly exploring new web technologies.
 
         Let’s Build Something Amazing Together! 🎯
