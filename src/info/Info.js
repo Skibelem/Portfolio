96689e3b34bebba04d279fe50a3bb09454628a9f
Joel Adelugba <adelugba.joel@gmail.com>
2025-07-04 09:34:21 +0100

    Changed the link for mock 1

--- src/info/Info.js
+++ src/info/Info.js
@@ -95,8 +95,8 @@ export const info = {
     portfolio: [ // This is where your portfolio projects will be detailed
         {
             title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+            live: "https://kiateka.netlify.app/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/skibelem", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
