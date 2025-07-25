55491df808177c73f31cf829d6682b08addedd12
Joel Adelugba <adelugba.joel@gmail.com>
2025-07-25 12:43:08 +0100

    Changed the garri to curiosity and also the frontend drafting

--- src/info/Info.js
+++ src/info/Info.js
@@ -35,7 +35,7 @@ export const info = {
     miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
         {
             emoji: '☕',
-            text: 'fueled by garri'
+            text: 'fueled by curriousity'
         },
         {
             emoji: '🌎',
